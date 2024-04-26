5846d3e977cd8dbb23ec0a67f5367917f90cfe2f
lemon1470 <128782692+lemon1470@users.noreply.github.com>
2024-04-26 15:04:02 +0900

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function showSavedText() {
       savedTextkoElement.textContent = "저장된 뜻이 없습니다";
     } else {
       savedObjectko.forEach(function(item, index) {
-        savedTextkoElement.innerHTML += item + "<br>";
+        savedTextkoElement.innerHTML +=  (index + 1) + ". "+item + "<br>";
       });
     }
 }
